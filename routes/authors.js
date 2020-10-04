63f2f7c9a8d476e9ce81a2cfad51b1cfbbe771e3
Dip Kumar Kapat <dip.eduapp@gmail.com>
2020-10-04 21:03:10 +0530

    Changes done

--- routes/authors.js
+++ routes/authors.js
@@ -5,11 +5,11 @@ const Author = require("../models/author");
 // All Authors Route
 router.get("/", async (req, res) => {
   let searchOptions = {};
-  if (req.query.name.trim() != null && req.query.name.trim() !== "") {
-    searchOptions.name.trim() = new RegExp(req.query.name.trim(), "i");
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
   }
   try {
-    const authors = await Author.find(searchOptions).trim();
+    const authors = await Author.find(searchOptions);
     res.render("authors/index", {
       authors: authors,
       searchOptions: req.query,
@@ -28,7 +28,7 @@ router.get("/new", (req, res) => {
 // Create Author Route
 router.post("/", async (req, res) => {
   const author = new Author({
-    name: req.body.name.trim,
+    name: req.body.name.trim(),
   });
   try {
     const newAuthor = await author.save().trim();
